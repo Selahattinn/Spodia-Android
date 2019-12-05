02c1b930fe6c37860b22fb1f49dd0e146bf587aa
furkansahinfs <furkansahin_fs@outlook.com>
2019-12-05 22:27:59 +0300

    Değişiklik
    Xml hataları giderildi. Toast sıkıntısı giderildi. Şu anlık hiçbir sıkıntı yok.

--- Spodia/app/src/main/java/com/example/spodia/Login.java
+++ Spodia/app/src/main/java/com/example/spodia/Login.java
@@ -2,7 +2,6 @@ package com.example.spodia;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -12,7 +11,6 @@ import android.widget.TextView;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.widget.Toast;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,100 +23,85 @@ import java.util.Map;
 
 
 
-public class Login extends Activity {
-    private EditText tvName;
-    private EditText tvPassword;
+public class Login extends Activity
+{
+    /**
+     * et = EditText - cb = CheckBox - tv = TextView - iv = ImageView
+     */
+    private EditText et_username;
+    private EditText et_password;
     private Button buttonLogin;
-    private CheckBox checkBox;
-    private TextView tvForgetPassword;
-    private ImageView ıvFacebookLogin;
-    private ImageView ıvTwitterLogin;
-    private ImageView ıvGmailLogin;
+    private CheckBox cb_rememberMe;
+    private TextView tv_ForgetPassword;
+    private ImageView iv_FacebookLogin;
+    private ImageView iv_TwitterLogin;
+    private ImageView iv_GmailLogin;
     private Button buttonSignUp;
-    Toast toast;
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         new NukeSSLCerts().nuke();
-        tvName =findViewById(R.id.name);
-        tvPassword=findViewById(R.id.password);
-        buttonLogin=findViewById(R.id.Login);
-        checkBox=findViewById(R.id.rememberMe);
-        tvForgetPassword=findViewById(R.id.forgetPassword);
-        ıvFacebookLogin=findViewById(R.id.facebook);
-        ıvTwitterLogin=findViewById(R.id.twitter);
-        ıvGmailLogin=findViewById(R.id.gmail);
-        buttonSignUp=findViewById(R.id.signUp);
+
+        et_username = findViewById(R.id.username);
+        et_password = findViewById(R.id.password);
+        buttonLogin = findViewById(R.id.Login);
+        cb_rememberMe = findViewById(R.id.rememberMe);
+        tv_ForgetPassword = findViewById(R.id.forgetPassword);
+        iv_FacebookLogin = findViewById(R.id.facebook);
+        iv_TwitterLogin = findViewById(R.id.twitter);
+        iv_GmailLogin = findViewById(R.id.gmail);
+        buttonSignUp = findViewById(R.id.signUp);
 
 
         buttonLogin.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View v)
+            public void onClick(View view)
             {
-                LoginTask simpleTask = new LoginTask();
-                simpleTask.execute("trying to login");
+                LoginTask loginTask = new LoginTask();
+                loginTask.execute("");
+            }
+        });
 
-                if(simpleTask.responseString != null)
-                {
-                    if (simpleTask.responseString.equals("Connected"))
-                    {
-                        toast.makeText(getApplicationContext(), "Login is succesfull.", Toast.LENGTH_SHORT);
-                        toast.show();
-                        Intent mainPage = new Intent(Login.this, MainPage.class);
-                        startActivity(mainPage);
-                    }
-                    else {
-                        toast.makeText(getApplicationContext(),"Invalid login,try again.",Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                }
-
-                else {
-                    toast.makeText(getApplicationContext(),"Control the internet connection!",Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-
-
-        }});
-
-        tvForgetPassword.setOnClickListener(new View.OnClickListener()
+        tv_ForgetPassword.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Intent forgetPassword =new Intent(Login.this,ForgetPassword.class);
+                Intent forgetPassword = new Intent(Login.this, ForgetPassword.class);
                 startActivity(forgetPassword);
             }
         });
 
-        ıvFacebookLogin.setOnClickListener(new View.OnClickListener()
+        iv_FacebookLogin.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Intent facebookLogin =new Intent(Login.this,FacebookLogin.class);
+                Intent facebookLogin = new Intent(Login.this, FacebookLogin.class);
                 startActivity(facebookLogin);
             }
         });
 
-        ıvTwitterLogin.setOnClickListener(new View.OnClickListener()
+        iv_TwitterLogin.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Intent twitterLogin =new Intent(Login.this,TwitterLogin.class);
+                Intent twitterLogin = new Intent(Login.this, TwitterLogin.class);
                 startActivity(twitterLogin);
             }
         });
 
-        ıvGmailLogin.setOnClickListener(new View.OnClickListener() {
+        iv_GmailLogin.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Intent gmailLogin =new Intent(Login.this,GmailLogin.class);
+            public void onClick(View v)
+            {
+                Intent gmailLogin = new Intent(Login.this, GmailLogin.class);
                 startActivity(gmailLogin);
             }
         });
@@ -128,68 +111,80 @@ public class Login extends Activity {
             @Override
             public void onClick(View v)
             {
-                Intent signIn =new Intent(Login.this,SignIn.class);
+                Intent signIn = new Intent(Login.this, SignIn.class);
                 startActivity(signIn);
             }
         });
     }
 
 
-
     private class LoginTask extends AsyncTask<String, Void, String>
     {
-        String responseString = null;
-        protected String doInBackground(String...voids)
+
+        protected String doInBackground(String... voids)
         {
 
 
-            String url="https://172.16.6.120:8080";
-
-            final StringRequest request=new StringRequest(Request.Method.POST,url,new Response.Listener<String>(){
+            String url = "https://10.8.58.3:8080";
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
 
                 @Override
-                public void onResponse(String response){
-
-                    if (!response.equals(null)) {
-                        responseString = response.intern();
+                public void onResponse(String response)
+                {
+                    if (response != null)
+                    {
+                        if (response.intern().equals("Connected"))
+                        {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Succesfull", Toast.LENGTH_SHORT);
+                            toast.show();
+                            Intent mainPage = new Intent(Login.this, MainPage.class);
+                            startActivity(mainPage);
+                        }
+                        else
+                        {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Invalid login.", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
                     }
-                    else {
-                        responseString = response.intern();
-
+                    else
+                    {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Connection poroblem.", Toast.LENGTH_SHORT);
+                        toast.show();
                     }
                 }
-            },new Response.ErrorListener(){
+            }, new Response.ErrorListener()
+            {
                 @Override
-                public void onErrorResponse(VolleyError error){
-                    Log.d(responseString,"hata");
+                public void onErrorResponse(VolleyError error)
+                {
+
                 }
 
             })
             {
                 @Override
-                public Map<String, String> getHeaders()throws AuthFailureError{
-                    Map<String, String> params=new HashMap<String, String>();
-                    params.put("User", tvName.getText().toString());
-                    params.put("Pass", tvPassword.getText().toString());
+                public Map<String, String> getHeaders() throws AuthFailureError
+                {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("User", et_username.getText().toString());
+                    params.put("Pass", et_password.getText().toString());
                     return params;
-
                 }
 
-                    /*
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<String, String>();
-                        params.put("User", "Furkan");
-                        params.put("Pass", "admin");
-                        return params;
-                    }*/
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("User", "Furkan");
+                    params.put("Pass", "admin");
+                    return params;
+                }
             };
 
-            RequestQueue queue=Volley.newRequestQueue(getApplicationContext());
+            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
             queue.add(request);
-
-            return responseString;
+           return null;
         }
-
     }
 }
+
